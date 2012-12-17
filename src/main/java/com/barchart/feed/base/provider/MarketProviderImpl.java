6a0f6056cf1b733a7ee85e6bc945608e83c0b684
Andrei.Pozolotin <Andrei.Pozolotin@gmail.com>
2012-12-17 13:14:25 -0600

    license

--- src/main/java/com/barchart/feed/base/provider/MarketProviderImpl.java
+++ src/main/java/com/barchart/feed/base/provider/MarketProviderImpl.java
@@ -1,4 +1,11 @@
 /**
+ * Copyright (C) 2011-2012 Barchart, Inc. <http://www.barchart.com/>
+ *
+ * All rights reserved. Licensed under the OSI BSD License.
+ *
+ * http://www.opensource.org/licenses/bsd-license.php
+ */
+/**
  * 
  */
 package com.barchart.feed.base.provider;
