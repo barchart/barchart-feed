198d4fc90ca0d6d9b330767c89c77b77526df781
Andrei.Pozolotin <Andrei.Pozolotin@gmail.com>
2013-02-04 17:25:10 -0600

    new enums

---
+++ barchart-feed-inst/src/main/java/com/barchart/feed/inst/enums/BookLiquidity.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (C) 2011-2012 Barchart, Inc. <http://www.barchart.com/>
+ *
+ * All rights reserved. Licensed under the OSI BSD License.
+ *
+ * http://www.opensource.org/licenses/bsd-license.php
+ */
+package com.barchart.feed.inst.enums;
+
+import com.barchart.util.values.api.Value;
+
+public enum BookLiquidity implements Value<BookLiquidity> {
+
+	/** no size book */
+	NONE, //
+
+	/** only default sizes */
+	DEFAULT, //
+
+	/** only implied sizes */
+	IMPLIED, //
+
+	/** both default + implied sizes */
+	COMBINED, //
+
+	;
+
+	public final byte ord = (byte) ordinal();
+
+	private static final BookLiquidity[] ENUM_VALUES = values();
+
+	public static final BookLiquidity fromOrd(final byte ord) {
+		return ENUM_VALUES[ord];
+	}
+
+	@Override
+	public BookLiquidity freeze() {
+		return this;
+	}
+
+	@Override
+	public boolean isFrozen() {
+		return true;
+	}
+
+	@Override
+	public boolean isNull() {
+		return this == NONE;
+	}
+
+}
