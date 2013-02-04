f0e150deec8a54a00a60eb29d634872b0b0cf694
Andrei.Pozolotin <Andrei.Pozolotin@gmail.com>
2013-02-04 17:04:09 -0600

    use interfaces as tag stores

--- barchart-feed-inst/src/main/java/com/barchart/feed/inst/api/InstrumentField.java
+++ barchart-feed-inst/src/main/java/com/barchart/feed/inst/api/InstrumentField.java
@@ -7,62 +7,61 @@ import com.barchart.util.values.api.PriceValue;
 import com.barchart.util.values.api.SizeValue;
 import com.barchart.util.values.api.TextValue;
 
-public final class InstrumentField {
-	
-	private InstrumentField() {
-		
-	}
-	
+public interface InstrumentField {
+
 	/** market identifier; must be globally unique; */
-	public static final Tag<SizeValue> GUID = Tag.create("ID", SizeValue.class);
-	
+	Tag<SizeValue> GUID = Tag.create("ID", SizeValue.class);
+
 	/** vendor */
-	public static final Tag<TextValue> VENDOR = Tag.create("VENDOR", TextValue.class);
-	
+	Tag<TextValue> VENDOR = Tag.create("VENDOR", TextValue.class);
+
 	/** market symbol; can be non unique; */
-	public static final Tag<TextValue> VENDOR_SYMBOL = Tag.create("SYMBOL", TextValue.class);
-	
+	Tag<TextValue> VENDOR_SYMBOL = Tag.create("SYMBOL", TextValue.class);
+
 	/** market free style description; can be used in full text search */
-	public static final Tag<TextValue> DESCRIPTION = Tag.create("DESCRIPTION", TextValue.class);
-	
+	Tag<TextValue> DESCRIPTION = Tag.create("DESCRIPTION", TextValue.class);
+
 	/** market originating exchange identifier */
-	public static final Tag<TextValue> EXCHANGE_ID = Tag.create("EXCHANGE_ID", TextValue.class);
-	
+	Tag<TextValue> EXCHANGE_ID = Tag.create("EXCHANGE_ID", TextValue.class);
+
 	/** book depth */
-	public static final Tag<SizeValue> BOOK_DEPTH = Tag.create("BOOK_SIZE", SizeValue.class);
-	
+	Tag<SizeValue> BOOK_DEPTH = Tag.create("BOOK_SIZE", SizeValue.class);
+
 	/** stock vs future vs etc. */
-	public static final Tag<CodeCFI> CFI_CODE = Tag.create("TYPE", CodeCFI.class);
-	
+	Tag<CodeCFI> CFI_CODE = Tag.create("TYPE", CodeCFI.class);
+
 	/** price currency */
-	public static final Tag<MarketCurrency> CURRENCY = Tag.create("CURRENCY", MarketCurrency.class);
-	
+	Tag<MarketCurrency> CURRENCY = Tag.create("CURRENCY", MarketCurrency.class);
+
 	/** price step / increment size / tick size */
-	public static final Tag<PriceValue> PRICE_STEP = Tag.create("PRICE_STEP", PriceValue.class);
-	
+	Tag<PriceValue> PRICE_STEP = Tag.create("PRICE_STEP", PriceValue.class);
+
 	/** value of a future contract / stock share */
-	public static final Tag<PriceValue> POINT_VALUE = Tag.create("PRICE_POINT", PriceValue.class);
-	
+	Tag<PriceValue> POINT_VALUE = Tag.create("PRICE_POINT", PriceValue.class);
+
 	/** display fraction base : decimal(10) vs binary(2), etc. */
-	public static final Tag<SizeValue> DISPLAY_BASE = Tag.create("DISPLAY_BASE", SizeValue.class);
-	
+	Tag<SizeValue> DISPLAY_BASE = Tag.create("DISPLAY_BASE", SizeValue.class);
+
 	/** display fraction exponent */
-	public static final Tag<SizeValue> DISPLAY_EXPONENT = Tag.create("DISPLAY_EXPONENT", SizeValue.class);
-	
+	Tag<SizeValue> DISPLAY_EXPONENT = Tag.create("DISPLAY_EXPONENT",
+			SizeValue.class);
+
 	/** lifetime of instrument */
-	public static final Tag<TimeInterval> LIFETIME = Tag.create("LIFETIME", TimeInterval.class);
-	
-	/** array of intervals of market hours in a normal week, denoted in minutes from Sunday morning */
-	public static final Tag<TimeInterval[]> MARKET_HOURS = Tag.create("MARKET_HOURS", TimeInterval[].class);
-	
+	Tag<TimeInterval> LIFETIME = Tag.create("LIFETIME", TimeInterval.class);
+
+	/**
+	 * array of intervals of market hours in a normal week, denoted in minutes
+	 * from Sunday morning
+	 */
+	Tag<TimeInterval[]> MARKET_HOURS = Tag.create("MARKET_HOURS",
+			TimeInterval[].class);
+
 	/** timezone represented as offset in minutes from utc */
-	public static final Tag<SizeValue> TIME_ZONE_OFFSET = Tag.create("TIME_ZONE", SizeValue.class);
-	
-	
-	public static final Tag<?>[] FIELDS = new Tag[] {
-		GUID, VENDOR, VENDOR_SYMBOL, EXCHANGE_ID, DESCRIPTION, BOOK_DEPTH, 
-		CFI_CODE, CURRENCY, PRICE_STEP, POINT_VALUE, DISPLAY_BASE, DISPLAY_EXPONENT,  
-		LIFETIME, MARKET_HOURS, TIME_ZONE_OFFSET
-	};
+	Tag<SizeValue> TIME_ZONE_OFFSET = Tag.create("TIME_ZONE", SizeValue.class);
+
+	Tag<?>[] FIELDS = new Tag[] { GUID, VENDOR, VENDOR_SYMBOL, EXCHANGE_ID,
+			DESCRIPTION, BOOK_DEPTH, CFI_CODE, CURRENCY, PRICE_STEP,
+			POINT_VALUE, DISPLAY_BASE, DISPLAY_EXPONENT, LIFETIME,
+			MARKET_HOURS, TIME_ZONE_OFFSET };
 
 }
