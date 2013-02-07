3dda299262f1dc18cebbfed802735e45f531d069
Andrei.Pozolotin <Andrei.Pozolotin@gmail.com>
2013-02-07 09:19:37 -0600

    license

--- barchart-feed-inst/src/test/java/com/barchart/feed/inst/provider/TestInstrument.java
+++ barchart-feed-inst/src/test/java/com/barchart/feed/inst/provider/TestInstrument.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright (C) 2011-2013 Barchart, Inc. <http://www.barchart.com/>
+ *
+ * All rights reserved. Licensed under the OSI BSD License.
+ *
+ * http://www.opensource.org/licenses/bsd-license.php
+ */
 package com.barchart.feed.inst.provider;
 
 import static com.barchart.feed.api.fields.InstrumentField.BOOK_DEPTH;
