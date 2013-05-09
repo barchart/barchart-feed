a45d387817029f3cb2d15e7d93baab6d6468b774
Andrei.Pozolotin <Andrei.Pozolotin@gmail.com>
2013-05-09 15:00:24 -0500

    review

--- api-review/src/main/java/com/barchart/feed/api/market/Agent.java
+++ api-review/src/main/java/com/barchart/feed/api/market/Agent.java
@@ -2,9 +2,12 @@ package com.barchart.feed.api.market;
 
 public interface Agent {
 
-	public void activate();
-	public void update();
-	public void deactivate();
-	public void dismiss();
-	
+	void activate();
+
+	void update();
+
+	void deactivate();
+
+	void dismiss();
+
 }
--- api-review/src/main/java/com/barchart/feed/api/market/Marketplace.java
+++ api-review/src/main/java/com/barchart/feed/api/market/Marketplace.java
@@ -4,7 +4,6 @@ import com.barchart.feed.api.data.client.MarketDataObject;
 import com.barchart.feed.api.data.framework.Instrument;
 import com.barchart.feed.api.data.framework.Market;
 import com.barchart.feed.api.exchange.Exchange;
-import com.barchart.feed.api.market.FrameworkElement;
 import com.barchart.feed.api.message.Message;
 import com.barchart.feed.api.util.Filter;
 import com.barchart.missive.api.Tag;
@@ -15,51 +14,79 @@ import com.barchart.missive.api.Tag;
 public interface Marketplace {
 
 	void attachAgent(FrameworkAgent<?> agent);
+
 	void updateAgent(FrameworkAgent<?> agent);
+
 	void detachAgent(FrameworkAgent<?> agent);
-	
+
 	void handle(Message message);
-	
+
 	<V extends FrameworkElement<V>> Builder<V> builder();
-	
-	public interface Builder<V extends FrameworkElement<V>> {
-		
-		public Builder<V> filter(String... symbols);
-		public Builder<V> filter(Instrument... instruments);
-		public Builder<V> filter(Exchange... exchange);
-		public Builder<V> filter(Class<Message>... message);
-		public Builder<V> filter(Filter<?>... filter);
-		
-		public <M extends MarketDataObject> Agent build(
-				MarketCallback<M> callback);
-		
-		public <M extends MarketDataObject> Agent build(
-				MarketCallback<M> callback, InstrumentFilter filter);
-		
+
+	interface Builder<V extends FrameworkElement<V>> {
+
+		/** FIXME resolution involved, throw exception on failure? */
+		Builder<V> filter(String... symbols);
+
+		Builder<V> filter(Instrument... instruments);
+
+		Builder<V> filter(Exchange... exchange);
+
+		Builder<V> filter(Class<Message>... message);
+
+		Builder<V> filter(Filter<?>... filter);
+
+		<M extends MarketDataObject> Agent build(MarketCallback<M> callback);
+
+		<M extends MarketDataObject> Agent build(MarketCallback<M> callback,
+				InstrumentFilter filter);
+
 	}
-	
+
 	/*
 	 * AKA "RegTaker"
 	 */
-	interface FrameworkAgent<V extends MarketDataObject> extends Agent, InstrumentFilter {
+	interface FrameworkAgent<V extends MarketDataObject> extends Agent,
+			InstrumentFilter {
 
 		void bindMarketplace(Marketplace marketplace);
-		
+
 		<M extends FrameworkElement<M>> MarketTag<M> callbackDataObjectTag();
-		
+
 		Tag<?>[] tagsToListenTo();
-		
+
 		// To be called by market on handle(MarketMessage)
 		// Agent can internally route callback based on message if needed
 		// And makes Market responsible for getting the data object to
 		// pass into the callback
-		<M extends FrameworkElement<M>> void handle(Market market, 
+		<M extends FrameworkElement<M>> void handle(Market market,
 				Message message, FrameworkElement<M> data);
-		
+
 		void attach(Market market);
+
 		void update(Market market);
+
 		void detach(Market market);
-		
+
+		//
+
+		@Override
+		void activate();
+
+		@Override
+		void update();
+
+		@Override
+		void deactivate();
+
+		@Override
+		void dismiss();
+
+		//
+
+		@Override
+		boolean filter(Instrument instrument);
+
 	}
-	
+
 }
