2f8e342be4d2ab6593d00b1a212516b13bf063d5
glitchfield <gavin.litchfield@barchart.com>
2013-06-17 12:35:54 -0500

    Added Metadata interface

---
+++ api/src/main/java/com/barchart/feed/api/data/Metadata.java
@@ -0,0 +1,12 @@
+package com.barchart.feed.api.data;
+
+/**
+ * Top level interface for metadata objects.  Current extensions are instrument
+ * and exchange.  Future extensions are channel and group.
+ * 
+ * @author Gavin M Litchfield
+ *
+ */
+public interface Metadata {
+
+}
