49a4ed8793328daf7f2a096986f3f921a38834cd
glitchfield <gavin.litchfield@barchart.com>
2013-07-22 12:05:11 -0500

    InstMap interface

--- inst/src/main/java/com/barchart/feed/inst/provider/InstrumentMap.java
+++ inst/src/main/java/com/barchart/feed/inst/provider/InstrumentMap.java
@@ -1,9 +1,53 @@
 package com.barchart.feed.inst.provider;
 
-import java.util.Map;
+import org.openfeed.proto.inst.InstrumentDefinition;
 
-import com.barchart.feed.api.model.meta.Instrument;
+public interface InstrumentMap {
 
-public interface InstrumentMap extends Map<String, Instrument> {
+	void clear();
+	
+	boolean containsKey(String key);
+	
+	InstrumentDefinition get(String symbol);
+	
+	void put(String key, InstrumentDefinition value);
+	
+	int size();
+	
+	void close();
+	
+	InstrumentMap NULL = new InstrumentMap() {
 
+		@Override
+		public void clear() {
+			
+		}
+
+		@Override
+		public boolean containsKey(String key) {
+			return false;
+		}
+
+		@Override
+		public InstrumentDefinition get(String symbol) {
+			return InstrumentDefinition.getDefaultInstance();
+		}
+
+		@Override
+		public void put(String key, InstrumentDefinition value) {
+			
+		}
+
+		@Override
+		public int size() {
+			return 0;
+		}
+
+		@Override
+		public void close() {
+			
+		}
+		
+	};
+	
 }
