8ea2cb82e251f2da9f3439c6e3e6df07536877d7
glitchfield <gavin.litchfield@barchart.com>
2013-12-12 09:22:07 -0600

    Bugfix for historical symbol formatting, added tests

--- base/src/main/java/com/barchart/feed/base/provider/Symbology.java
+++ base/src/main/java/com/barchart/feed/base/provider/Symbology.java
@@ -294,6 +294,7 @@ public final class Symbology {
 		
 	}
 
+	// FIXME
 	public static String formatHistoricalSymbol(String symbol) {
 		
 		if(symbol == null) {
@@ -313,6 +314,13 @@ public final class Symbology {
 		if(!Character.isDigit(symbol.charAt(symbol.length() - 2))) {
 			return new StringBuilder(symbol).insert(symbol.length() - 1, O).toString();
 		}
+		
+		/* e.g. ESH2013 */
+		if(Character.isDigit(symbol.charAt(symbol.length() - 3))) {
+			return new StringBuilder(symbol).delete(symbol.length() - 4, 
+					symbol.length() - 2).toString();
+		}
+		
 		
 		return symbol;
 	}
