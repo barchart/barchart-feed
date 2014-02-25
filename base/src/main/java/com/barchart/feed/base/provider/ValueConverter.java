e35b0e1aaa2d2a2ae96bfbe724accc1731072a8a
glitchfield <gavin.litchfield@barchart.com>
2014-02-25 15:42:25 -0600

    Null check in value converter

--- base/src/main/java/com/barchart/feed/base/provider/ValueConverter.java
+++ base/src/main/java/com/barchart/feed/base/provider/ValueConverter.java
@@ -17,22 +17,37 @@ public final class ValueConverter {
 	private static final ValueFactory factory = new ValueFactoryImpl();
 	
 	public static Price price(final PriceValue value) {
+		if(value.isNull()) {
+			return Price.NULL;
+		}
 		return factory.newPrice(value.mantissa(), value.exponent());
 	}
 	
 	public static Size size(final SizeValue value) {
+		if(value.isNull()) {
+			return Size.NULL;
+		}
 		return factory.newSize(value.asLong(), 0);
 	}
 	
 	public static Time time(final TimeValue value) {
+		if(value.isNull()) {
+			return Time.NULL;
+		}
 		return factory.newTime(value.asMillisUTC());
 	}
 	
 	public static Fraction fraction(final com.barchart.feed.base.values.api.Fraction frac) {
+		if(frac.isNull()) {
+			return Fraction.NULL;
+		}
 		return factory.newFraction((int)frac.base(), frac.exponent());
 	}
 	
 	public static Bool bool(final BooleanValue value) {
+		if(value.isNull()) {
+			return Bool.NULL;
+		}
 		return factory.newBoolean(value.asBoolean());
 	}
 
