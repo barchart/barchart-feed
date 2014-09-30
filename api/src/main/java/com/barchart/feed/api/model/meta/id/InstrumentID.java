2e56281ae08f0abd4bdc5a5ae1da901dd205416c
jjongsma <jeremy@barchart.com>
2014-09-30 11:29:32 -0500

    Added long constructor

--- api/src/main/java/com/barchart/feed/api/model/meta/id/InstrumentID.java
+++ api/src/main/java/com/barchart/feed/api/model/meta/id/InstrumentID.java
@@ -9,16 +9,20 @@ public class InstrumentID extends MetadataID<InstrumentID> {
 	public InstrumentID(final String id) {
 		super(id, InstrumentID.class);
 	}
-	
+
+	public InstrumentID(final long id) {
+		super(String.valueOf(id), InstrumentID.class);
+	}
+
 	public static final InstrumentID NULL = new InstrumentID("NULL_INSTRUMENT_ID") {
-		
+
 		@Override
 		public boolean isNull() {
 			return true;
 		}
-		
+
 	};
-	
+
 	@Override
 	public Metadata.MetaType metaType() {
 		return MetaType.INSTRUMENT;
