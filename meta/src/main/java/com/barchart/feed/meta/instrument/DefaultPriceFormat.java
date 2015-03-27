89c2e948d7b7771be81c075257be6fe8c6e754d6
glitchfield <gavin.litchfield@barchart.com>
2015-03-27 11:00:51 -0400

    Added Proto Constructor for DefaultPriceFormat

--- meta/src/main/java/com/barchart/feed/meta/instrument/DefaultPriceFormat.java
+++ meta/src/main/java/com/barchart/feed/meta/instrument/DefaultPriceFormat.java
@@ -1,5 +1,9 @@
 package com.barchart.feed.meta.instrument;
 
+import java.nio.channels.UnresolvedAddressException;
+
+import org.openfeed.InstrumentDefinition;
+
 import com.barchart.feed.api.model.meta.instrument.PriceFormat;
 import com.barchart.util.value.api.Price;
 
@@ -13,12 +17,20 @@ public class DefaultPriceFormat implements PriceFormat {
 	private final int subPrecision;
 	private final SubFormat subFormat;
 
-	public DefaultPriceFormat(final int denominator_, final int precision_, final boolean fractional_) {
+	public DefaultPriceFormat(
+			final int denominator_, 
+			final int precision_, 
+			final boolean fractional_) {
 		this(denominator_, precision_, fractional_, 0, 0, SubFormat.FLAT);
 	}
 
-	public DefaultPriceFormat(final int denominator_, final int precision_, final boolean fractional_,
-			final int subDenominator_, final int subPrecision_, final SubFormat subFormat_) {
+	public DefaultPriceFormat(
+			final int denominator_, 
+			final int precision_, 
+			final boolean fractional_,
+			final int subDenominator_, 
+			final int subPrecision_, 
+			final SubFormat subFormat_) {
 
 		denominator = denominator_;
 		precision = precision_;
@@ -27,6 +39,35 @@ public class DefaultPriceFormat implements PriceFormat {
 		subPrecision = subPrecision_;
 		subFormat = subFormat_;
 
+	}
+	
+	public DefaultPriceFormat(final InstrumentDefinition.PriceFormat format) {
+		
+		if(format == null || !format.isInitialized()) {
+			throw new IllegalArgumentException("Bad format proto");
+		}
+		
+		denominator = format.getDenominator();
+		precision = format.getPrecision();
+		fractional = format.getIsFractional();
+		subDenominator = format.getSubDenominator();
+		subPrecision = format.getSubPrecision();
+		
+		switch(format.getSubFormat()) {
+		default:
+			throw new IllegalArgumentException("Unknown Price Format " + format.getSubFormat());
+		case DECIMAL:
+			subFormat = SubFormat.DECIMAL;
+			break;
+		case FLAT:
+			subFormat = SubFormat.FLAT;
+			break;
+		case FRACTIONAL:
+			subFormat = SubFormat.FRACTIONAL;
+			break;
+			
+		}
+		
 	}
 
 	@Override
